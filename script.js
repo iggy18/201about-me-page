1ec580a6f2e0fe9d4436ea1e8b66760a80189dab
seth mcfeeters <seth.mcfeeters@gmail.com>
2020-09-02 10:09:59 -0700

    made yes or no

--- script.js
+++ script.js
@@ -10,42 +10,42 @@ var game = confirm('would you like to play a guessing game?');
         alert('well whatever, nevermind');
     }
 
-var guess1 = prompt('where did I live before seattle?').toLowerCase();
-    if (guess1 === 'mississippi') {
+var guess1 = prompt('answer yes, or no. did I used to live in Mississippi?').toLowerCase();
+    if (guess1 === 'yes') {
         // console.log('thats correct');
         alert('thats correct!');
     } else {
-        alert('oops! the correct answer was Mississippi');
+        alert('oops! we must not have met, my southern charm is hard to miss');
     }
 
-var guess2 = prompt('what is my favorite athletic pastime?').toLowerCase();
-    if (guess2 === 'snowboarding') {
+var guess2 = prompt('is snowbording my favorite athletic pastime?').toLowerCase();
+    if (guess2 === 'yes') {
         // console.log('thats correct');
         alert('thats correct!');
     } else {
-        alert('oh no! the correct answer is snowboarding');
+        alert('oh no! are you saying I look more like a skiier?!');
     }
 
-var guess3 = prompt('what coding school am I attending?').toLowerCase();
-    if (guess3 === 'codefellows') {
+var guess3 = prompt('am I currently attending codefellows?').toLowerCase();
+    if (guess3 === 'yes') {
         // console.log('thats correct');
         alert('thats correct!');
     } else {
-        alert('bummer! the correct answer is codefellows');
+        alert('I wouldnt be able to quiz you if I wasnt ');
     }
 
-var guess4 = prompt('where did I work before I came to codefellows?').toLowerCase();
-    if (guess4 === 'victoria clipper') {
+var guess4 = prompt('did I work on board the Victoria Clipper?').toLowerCase();
+    if (guess4 === 'yes') {
         // console.log('thats correct');
         alert('thats correct!');
     } else {
-        alert('soory! the correct answer is victoria clipper, eh');
+        alert('soory! what are you on aboot? I was an onboard attendant, eh');
     }
 
-var guess5 = prompt('who do I hope to make proud?').toLowerCase();
-    if (guess5 === 'mom') {
+var guess5 = prompt('do I hope to make my mom proud?').toLowerCase();
+    if (guess5 === 'yes') {
         // console.log('thats correct');
-        alert('thats correct! thanks for playing, ' + user);
+        alert('thats correct! thanks for playing, <3 LOVE YOU MOM!' + user);
     } else {
-        alert('the correct answer is my mom <3 thanks for playing, ' + user);
+        alert(' I do want to make my mom proud! <3 LOVE YOU MOM! thanks for playing ' + user);
     }
