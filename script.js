38c853fb3107a28b4cef0374da3afec3621d93bd
Sowmya Billakanti <sowmya.billakanti8@gmail.com>
2020-09-03 17:05:11 -0700

    functions

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ var game = confirm('would you like to play a guessing game?');
         alert('well whatever, nevermind');
     }
 
+function question1() {
 var guess1 = prompt('answer yes, or no. did I used to live in Mississippi?').toLowerCase();
     if (guess1 === 'yes') {
         score = score + 1;
@@ -20,7 +21,11 @@ var guess1 = prompt('answer yes, or no. did I used to live in Mississippi?').toL
     } else {
         alert('oops! we must not have met, my southern charm is hard to miss');
     }
+}
 
+question1();
+
+function question2() {
 var guess2 = prompt('is snowbording my favorite athletic pastime?').toLowerCase();
     if (guess2 === 'yes') {
         score = score + 1;
@@ -29,7 +34,11 @@ var guess2 = prompt('is snowbording my favorite athletic pastime?').toLowerCase(
     } else {
         alert('oh no! are you saying I look more like a skiier?!');
     }
+}
 
+question2();
+
+function question3() {
 var guess3 = prompt('am I currently attending codefellows?').toLowerCase();
     if (guess3 === 'yes') {
         score = score + 1;
@@ -38,7 +47,11 @@ var guess3 = prompt('am I currently attending codefellows?').toLowerCase();
     } else {
         alert('I wouldnt be able to quiz you if I wasnt ');
     }
+}
 
+question3();
+
+function question4() { 
 var guess4 = prompt('did I work on board the Victoria Clipper?').toLowerCase();
     if (guess4 === 'yes') {
         score = score + 1;
@@ -47,7 +60,11 @@ var guess4 = prompt('did I work on board the Victoria Clipper?').toLowerCase();
     } else {
         alert('soory! what are you on aboot? I was an onboard attendant, eh');
     }
+}
 
+question4();
+
+function question5() {
 var guess5 = prompt('do I hope to make my mom proud?').toLowerCase();
     if (guess5 === 'yes') {
         score = score + 1;
@@ -56,6 +73,9 @@ var guess5 = prompt('do I hope to make my mom proud?').toLowerCase();
     } else {
         alert(' I do want to make my mom proud! <3 LOVE YOU MOM! thanks for playing ' + user);
     }
+}
+
+question5();
 
 // var number = Math.floor(Math.random() * 10 + 1);
 // for (var i = 0; i < 4 ; i++) {
@@ -73,6 +93,7 @@ var guess5 = prompt('do I hope to make my mom proud?').toLowerCase();
 //     }
 // }
 
+function question6() {
 var correctNumber = Math.floor(Math.random() * 10 + 1);
 var guesses = 0;
 while (guesses <= 4) {
@@ -89,8 +110,13 @@ while (guesses <= 4) {
         alert('too high');
     }
     guesses++
+}
 
 }
+
+question6();
+
+function question7() {
 var ray = ['eyes', 'nose', 'mouth', 'lips', 'ears', 'beard', 'freckles', 'mole', 'scar', 'pimple', 'pimples']
 // var count = 0
 
@@ -107,4 +133,7 @@ for (var i = 0; i < 6; i++) {
     }
 
 }
+}
+question7();
+
 alert('congrats! youre done answering questions. you got ' + score + ' correct answers!')
